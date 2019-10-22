116ea0376e05004865bdf6b643eba4eafd7668fa
lombokcyberlc <drivehadiwijaya@gmail.com>
2019-10-22 20:36:02 +0800

    rest api codeigniter

---
+++ application/controllers/File.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script allowed');
+
+/*----------------------------------------REQUIRE THIS PLUGIN----------------------------------------*/
+require APPPATH . '/libraries/REST_Controller.php';
+//use Restserver\Libraries\REST_Controller;
+
+class File extends REST_Controller{
+	/*----------------------------------------CONSTRUCTOR----------------------------------------*/
+	function __construct($config = 'rest'){
+		parent::__construct($config);
+		$this->load->database();
+	}
+
+	/*----------------------------------------GET KONTAK----------------------------------------*/
+	function index_get(){
+		$id = $this->get('id');
+
+		if($id == ''){
+			$query = $this->db->get('ppid')->result_array();
+		} else {
+			$this->db->where('id', $id);
+			$query = $this->db->get('ppid')->result_array();
+		}
+
+		$this->response($query, 200);
+	}
+
+	function index_post(){
+		$id = $this->post('id');
+
+		if($id == ''){
+			$query = $this->db->get('ppid')->result_array();
+		} else {
+			$this->db->where('id', $id);
+			$query = $this->db->get('ppid')->result_array();
+		}
+
+		$this->response($query, 200);
+	}
+}
+
+?>
